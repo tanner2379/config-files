2ea2d34ade12f814430713bfbf86dfceadef7943
tanner2379 <35666231+tanner2379@users.noreply.github.com>
2021-09-23 19:42:02 -0700

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,19 +1,28 @@
 import XMonad
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
 import XMonad.Util.Ungrab
+import XMonad.Hooks.ManageHelpers (isDialog, doFullFloat, doCenterFloat)
 import System.IO
+
+myManageHook::ManageHook
+myManageHook = composeAll
+  [ className =? "gimp" --> doFullFloat
+  , className =? "gmrun" --> doCenterFloat
+  , isDialog --> doFloat
+  ]
 
 main = do
   xmonad $ defaultConfig
     { terminal = "alacritty"
+    , manageHook = myManageHook
     } `additionalKeys`
-    [ ((shiftMask, xK_Print), ungrab *> spawn "scrot -s -e 'mv $f ~/Pictures/Screenshots/'")
+    [ ((shiftMask, xK_Print), unGrab *> spawn "scrot -s -e 'mv $f ~/Pictures/Screenshots/'")
     , ((0, xK_Print), spawn "scrot -e 'mv $f ~/Pictures/Screenshots/'")
-    , ("M-f", spawn "firefox")
-    , ("M-t", spawn terminal)
-    , ("M-g", spawn "gmrun")
     , ((mod4Mask, xK_Left), spawn "amixer set Master 5%-")
     , ((mod4Mask, xK_Right), spawn "amixer set Master 5%+")
-
+    ] `additionalKeysP`
+    [ ("M-f", spawn "firefox")
+    , ("M-<Return>", spawn "alacritty")
+    , ("M-g", spawn "gmrun")
     ]

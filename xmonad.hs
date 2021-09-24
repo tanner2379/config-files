953bd11fe1d0fadc94993e9ef015432844203778
tanner2379 <35666231+tanner2379@users.noreply.github.com>
2021-09-24 09:09:06 -0700

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
 import XMonad.Util.Ungrab
 import XMonad.Hooks.ManageHelpers (isDialog, doFullFloat, doCenterFloat)
+import XMonad.Util.Brightness(Brightness)
 import System.IO
 
 myManageHook::ManageHook
@@ -21,6 +22,8 @@ main = do
     , ((0, xK_Print), spawn "scrot -e 'mv $f ~/Pictures/Screenshots/'")
     , ((mod4Mask, xK_Left), spawn "amixer set Master 5%-")
     , ((mod4Mask, xK_Right), spawn "amixer set Master 5%+")
+    , ((mod4Mask, xK_Up), Brightness.increase)
+    , ((mod4Mask, xK_Down), Brightness.decrease)
     ] `additionalKeysP`
     [ ("M-f", spawn "firefox")
     , ("M-a", spawn "alacritty")

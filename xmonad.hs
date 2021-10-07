20e275b98ae1e37a4aeee409b06a1db2ebe31f9e
tanner2379 <35666231+tanner2379@users.noreply.github.com>
2021-10-06 22:33:26 -0700

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,16 @@
+import System.IO
 import XMonad
+import Graphics.X11.ExtraTypes.XF86
+
+import XMonad.Util.SpawnOnce
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
 import XMonad.Util.Ungrab
+
 import XMonad.Hooks.ManageHelpers (isDialog, doFullFloat, doCenterFloat)
-import Graphics.X11.ExtraTypes.XF86
-import System.IO
+
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Grid
 
 myManageHook::ManageHook
 myManageHook = composeAll
@@ -13,10 +19,25 @@ myManageHook = composeAll
   , isDialog --> doFloat
   ]
 
+myLayout = ResizableTall 1 (3/100) (1/2) [] ||| Grid
+  
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "~/.fehbg &"
+  spawnOnce "xsetroot -cursor_name Left_ptr &"
+  spawnOnce "lux -S 10% &"
+
+
+myTerminal :: String
+myTerminal = "alacritty"
+
 main = do
   xmonad $ defaultConfig
-    { terminal = "alacritty"
+    { terminal = myTerminal
     , manageHook = myManageHook
+    , layoutHook = myLayout
+    , startupHook = myStartupHook
     } `additionalKeys`
     [ ((shiftMask, xK_Print), unGrab *> spawn "scrot -s -e 'mv $f ~/Pictures/Screenshots/'")
     , ((0, xK_Print), spawn "scrot -e 'mv $f ~/Pictures/Screenshots/'")
@@ -24,8 +45,11 @@ main = do
     , ((mod4Mask, xK_Right), spawn "amixer set Master 5%+")
     , ((mod4Mask, xK_Up), spawn "lux -a 10%")
     , ((mod4Mask, xK_Down), spawn "lux -s 10%")
+    , ((mod1Mask, xK_b), sendMessage MirrorShrink)
+    , ((mod1Mask, xK_u), sendMessage MirrorExpand)
     ] `additionalKeysP`
     [ ("M-f", spawn "firefox")
-    , ("M-a", spawn "alacritty")
+    , ("M-a", spawn myTerminal)
     , ("M-g", spawn "gmrun")
     ]
+

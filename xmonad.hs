012f0854186f0fa7f8694ba720f6a6226d743c87
tanner2379 <35666231+tanner2379@users.noreply.github.com>
2021-10-09 23:42:56 -0700

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -4,26 +4,28 @@ import Graphics.X11.ExtraTypes.XF86
 
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
+import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Ungrab
 
 import XMonad.Hooks.ManageHelpers (isDialog, doFullFloat, doCenterFloat)
 
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Grid
+import XMonad.Layout.Column
 
 myManageHook::ManageHook
 myManageHook = composeAll
-  [ className =? "gimp" --> doFullFloat
+  [ className =? "Gimp" --> doFullFloat
   , className =? "gmrun" --> doCenterFloat
   , isDialog --> doFloat
   ]
 
-myLayout = ResizableTall 1 (3/100) (1/2) [] ||| Grid
+myLayout = ResizableTall 1 (3/100) (1/2) [] ||| Grid ||| Column 1.8
   
 
 myStartupHook :: X ()
 myStartupHook = do
+  spawnOnce "~/.screenlayout/default.sh &"
   spawnOnce "~/.fehbg &"
   spawnOnce "xsetroot -cursor_name Left_ptr &"
   spawnOnce "lux -S 10% &"
@@ -45,11 +47,9 @@ main = do
     , ((mod4Mask, xK_Right), spawn "amixer set Master 5%+")
     , ((mod4Mask, xK_Up), spawn "lux -a 10%")
     , ((mod4Mask, xK_Down), spawn "lux -s 10%")
-    , ((mod1Mask, xK_b), sendMessage MirrorShrink)
-    , ((mod1Mask, xK_u), sendMessage MirrorExpand)
-    ] `additionalKeysP`
-    [ ("M-f", spawn "firefox")
-    , ("M-a", spawn myTerminal)
-    , ("M-g", spawn "gmrun")
+    , ((mod1Mask .|. shiftMask, xK_l), sendMessage MirrorShrink)
+    , ((mod1Mask .|. shiftMask, xK_h), sendMessage MirrorExpand)
+    , ((mod1Mask, xK_f), spawn "firefox")
+    , ((mod1Mask, xK_a), spawn myTerminal)
+    , ((mod1Mask, xK_g), spawn "gmrun")
     ]
-
